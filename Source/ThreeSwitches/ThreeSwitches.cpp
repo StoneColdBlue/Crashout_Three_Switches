b679f9d8bc0c2d28f26610ed0f3206eb5bcb61b8
CactusFamilyTree <141030665+StoneColdBlue@users.noreply.github.com>
2024-10-05 12:56:52 +0200

    First push
    project folder

---
+++ Source/ThreeSwitches/ThreeSwitches.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ThreeSwitches.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ThreeSwitches, "ThreeSwitches" );
