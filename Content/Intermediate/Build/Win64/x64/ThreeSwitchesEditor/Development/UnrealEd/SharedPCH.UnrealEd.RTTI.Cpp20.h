1685e1b7d8bcb71425d1db5c2f96e84ace13181a
CactusFamilyTree <141030665+StoneColdBlue@users.noreply.github.com>
2024-11-26 17:52:14 +0200

    Respawn and timer
    The Ai and the player have a respawn now. They are able to kill each other there is still no count or pick up for ammo. There is a timer for the match to end after 5 minutes. Home page and end screen is added. no pause menu yet.

---
+++ Content/Intermediate/Build/Win64/x64/ThreeSwitchesEditor/Development/UnrealEd/SharedPCH.UnrealEd.RTTI.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
+#include "C:/Users/user/OneDrive/Documents/Akilan Projects/Game Dev/POE/Part 1/ThreeSwitches/Intermediate/Build/Win64/x64/ThreeSwitchesEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.RTTI.Cpp20.h"
+#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
